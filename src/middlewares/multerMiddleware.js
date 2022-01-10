4bdf064a5e7eef4cd21c6384d8b4c42ee888aedb
Julianarieltorres998 <julianarieltorres@gmail.com>
2022-01-10 11:33:58 -0300

    Añadido Multer para Usuarios y cambios menores

--- src/middlewares/multerMiddleware.js
+++ src/middlewares/multerMiddleware.js
@@ -10,6 +10,17 @@ const storage = multer.diskStorage({
     }
 })
 
+const userStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.resolve(__dirname, "../../public/images/iconosUsuarios"))
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname))
+    }
+})
+
 const upload = multer({storage});
+const userIconsUpload = multer({userStorage});
 
 module.exports = upload;
+module.exports = userIconsUpload;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const usuariosSubida = require('../middlewares/multerMiddleware');
 const validacionesMiddleware = require('../middlewares/validatorMiddleware');
 
 router.get('/login', userController.login)
@@ -9,6 +10,6 @@ router.get('/register', userController.register)
 
 router.get('/editUser', userController.editUser);
 
-router.post('/register/process', validacionesMiddleware, userController.registerProcess);
+router.post('/register', validacionesMiddleware, userController.registerProcess);
 
 module.exports = router
