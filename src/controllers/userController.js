83954512793b6e84da7f2cf4c36ad86121be0940
jhonnaa <jonnyemeni@gmail.com>
2022-01-24 10:48:21 -0300

    clearcookie

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -85,6 +85,7 @@ const userController ={
         })
     },
     logout: (req, res) => {
+        res.clearCookie('userEmail')
         req.session.destroy()
         return res.redirect('/')
     }
